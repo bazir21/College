34373c0a52d2ce07fc6c6eb61ca079ab1d2911ff
Bair Buyrchiyev <60492647+bazir21@users.noreply.github.com>
2020-02-26 19:27:20 +0000

    Finished all tests

--- Second Year/Algorithms and Data Structures/SortComparison/src/SortComparisonTest.java
+++ Second Year/Algorithms and Data Structures/SortComparison/src/SortComparisonTest.java
@@ -3,7 +3,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,9 +15,47 @@ import org.junit.runners.JUnit4;
 /**
  *  Test class for SortComparison.java
  *
- *  @author
+ *  @author	 Bair Buyrchiyev
  *  @version HT 2020
+ *									Insert		Selection		MergeRecursive		MergeIterative		Quick
+ *	numbers10						0.33ms		0ms				0.33ms				0ms					0ms
+ *	numbers100						0.33ms		0.33ms			0					0.33ms				0ms
+ *	numbers1000						1.33ms		2ms				0.33ms				0.33ms				0.33ms
+ *	numbers1000Duplicates			2ms			2ms				0.66ms				0.33ms				0.33ms
+ *	numbersNearlyOrdered1000		0ms			0ms				0ms					0ms					5ms
+ *	numbersReverse1000				0.66ms		0ms				0ms					0ms					0ms
+ *	numbersSorted1000				0.33ms		2ms				0.66ms				0ms					0ms
+ *`	AVERAGE RUN TIME:				0.714ms		0.9ms			0.285ms				0.142ms				0.808ms
+ *
+ *
+ *
+ *
+ *	a. Which of the sorting algorithms does the order of input have an impact on? Why?
+ *			
+ *			Quicksort, because it will check the pivot value against each value, which in this case is already sorted so it will run through every single value
+ *
+ *
+ *	b. Which algorithm has the biggest difference between the best and worst performance, based
+ *		on the type of input, for the input of size 1000? Why?
+ *			
+ *			SelectionSort has the biggest difference based on the size of the input
+ *			
+ *	c. Which algorithm has the best/worst scalability, i.e., the difference in performance time
+ *		based on the input size? Please consider only input files with random order for this answer.
+ *		
+ *			SelectionSort has the worst scalability as it went from 0ms to 2ms run time
+ *			QuickSort and MergeSortIterative have the best scalability as it only went up to 0.33ms each for 1000 items
+ *		
+ *	d. Did you observe any difference between iterative and recursive implementations of merge
+ *		sort?
+ *		
+ *			MergeSortRecursive ran longer on the already sorted array and with the duplicates
+ *		
+ *	e. Which algorithm is the fastest for each of the 7 input files?
+ *		
+ *			MergeSortIterative
  */
+
 @RunWith(JUnit4.class)
 public class SortComparisonTest
 {
@@ -89,13 +129,65 @@ public class SortComparisonTest
     /**
      *  Main Method.
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     * @throws IOException 
      *
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException 
     {
-        File file = new File("numbers10.txt");
-        BufferedReader reader = new BufferedReader(new FileReader("numbers10.txt"));
+        File file = new File(".\\src\\numbersSorted1000.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        double[] array = new double[1000];
+        int i = 0;
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+        	array[i++] = Double.parseDouble(line);
+        }
+        double timeStart = 0, timeEnd = 0;
         
+        double[] testArray = array;
+        timeStart = System.currentTimeMillis();
+        for (i = 0; i < 3; i++) {
+        	SortComparison.insertionSort(testArray);
+        }
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Insertion Sort: " + ((timeEnd - timeStart) / 3));
+        
+        
+        testArray = array;
+        timeStart = System.currentTimeMillis();
+        for (i = 0; i < 3; i++) {;
+        	SortComparison.selectionSort(testArray);
+        }
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Selection Sort: " + ((timeEnd - timeStart) / 3));
+        
+        
+        testArray = array;
+        timeStart = System.currentTimeMillis();
+        for (i = 0; i < 3; i++) {
+        	SortComparison.quickSort(testArray);
+        }
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Quick Sort: " + ((timeEnd - timeStart) / 3));
+        
+        
+        testArray = array;
+        timeStart = System.currentTimeMillis();
+        for (i = 0; i < 3; i++) {
+        	SortComparison.mergeSortIterative(testArray);
+        }
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Merge Sort Iterative: " + ((timeEnd - timeStart) / 3));
+        
+        
+        testArray = array;
+        timeStart = System.currentTimeMillis();
+        for (i = 0; i < 3; i++) {
+        	SortComparison.mergeSortRecursive(testArray);
+        }
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Merge Sort Recursive: " + ((timeEnd - timeStart) / 3));
+        
+        reader.close();
     }
 
 }
